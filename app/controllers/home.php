4feac3e773d92363fd20c1a14c8078307fd58016
Amen Khaled <aminabdo8@gmail.com>
2022-10-01 22:09:42 +0200

    added frontend and backend files and made path to them by "Assets"

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,8 +1,7 @@
 <?php
 class Home extends Controller {
    public function index($a = '',$b = ''){
-       print_r($a);
-       print_r($b);
-       return $this->view('home');
+
+       return $this->view('eshop/index');
    }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 session_start();
+
+$a = $_SERVER['REQUEST_SCHEME']."://". $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
+$asset = str_replace('index.php','',$a);
+define('ROOT',$asset);
+define('ASSETS',$asset ."assets/");
 include "../app/init.php";
